843c941d19165436630538c4ffd47eccccc71a07
Lena Ansari <lena.ansari@gmail.com>
2023-01-07 11:56:02 +0100

    model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get 'movies', to: 'movies#index'
+  get 'lists', to: 'lists#index'
+  get 'bookmarks', to: 'bookmarks#index'
   # root "articles#index"
 end
